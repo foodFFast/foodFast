5ba7fb5379ccc8f6660998f37f162ae0fc4278b7
Guido Queiroz <Takzzg@hotmail.com>
2022-06-07 02:21:26 -0300

    action types.js fixed

--- client/src/redux/actions/types.js
+++ client/src/redux/actions/types.js
@@ -1,4 +1,4 @@
-export const MAIN_TEST = "main_test"
-export const ASYNC_TEST_1 = "async_action_1"
-export const SYNC_TEST_1 = "sync_action_1"
 export const RESET_TESTS = "reset_tests"
+export const MAIN_TEST = "main_test"
+export const ASYNC_TEST_1 = "async_test_1"
+export const SYNC_TEST_1 = "sync_test_1"
