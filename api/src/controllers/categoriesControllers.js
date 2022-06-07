4de0113307d1ecc20d522d85ac4220ff5aab3f0e
emmaSDNZ <isaias.e.sudanez@gmail.com>
2022-06-07 17:10:24 -0300

    productos por categorias

--- api/src/controllers/categoriesControllers.js
+++ api/src/controllers/categoriesControllers.js
@@ -1,4 +1,5 @@
 import Categories from '../models/category.js'
+import Product from '../models/product.js'
 
 
 export const categories = async(req,res)=>{
@@ -7,5 +8,19 @@ export const categories = async(req,res)=>{
         return res.json(user)
     } catch (error) {
         console.log(error)
+        return res.json({error: "Error de servidor"})
+    }
+}
+
+export const category = async(req,res)=>{
+    const name = req.query.name
+    try {
+        if(!name) return res.json({error: "query invalid"})
+        const categories = await Product.find({category : name}) 
+        if(!categories) return res.json({error: "not found category"})
+        return res.json(categories)    
+    } catch (error) {
+        console.log(error)
+        return res.json({error: "Error de servidor"})   
     }
 }
--- api/src/routes/categoriesRoute.js
+++ api/src/routes/categoriesRoute.js
@@ -1,10 +1,14 @@
 import express from 'express';
-import { categories } from '../controllers/categoriesControllers.js'
+import { categories, category } from '../controllers/categoriesControllers.js'
 
 const router = express.Router()
 
-
+//GET: http://localhost:3000/api/v1/categories
 router.get('/',categories)
+
+//GET: http://localhost:3000/api/v1/categories/category?name=merienda
+router.get('/category', category)
+
 
 
 export default router;
