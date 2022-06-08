73d9756afc1529361393c17cefc005e25abe11ec
Guido Queiroz <Takzzg@hotmail.com>
2022-06-08 17:03:30 -0300

    scroll to top on link clicked

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,5 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { useEffect } from "react"
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom"
 
 import {
     MainTest,
@@ -20,6 +21,15 @@ import {
     NavBar,
     SingleCat
 } from "./components/index"
+
+const ScrollToTop = (props) => {
+    const location = useLocation()
+    useEffect(() => {
+        window.scrollTo(0, 0)
+    }, [location])
+
+    return <>{props.children}</>
+}
 
 const productsNestedRoutes = () => (
     <Route path="products">
@@ -46,56 +56,61 @@ function App() {
     return (
         <div className="App">
             <BrowserRouter>
-                {/* <Link to="/">
+                <ScrollToTop>
+                    {/* <Link to="/">
                     <h1>Food Fast</h1>
                 </Link>
                 <Link to="/auth">Auth</Link> */}
 
-                <div style={{ zIndex: 10, position: "sticky", top: 0 }}>
-                    <NavBar />
-                </div>
+                    <div style={{ zIndex: 10, position: "sticky", top: 0 }}>
+                        <NavBar />
+                    </div>
 
-                <Routes>
-                    <Route path="/">
-                        <Route index element={<Landing />} />
-                        <Route path="shops" element={<Shops />} />
-                        <Route path="categories">
-                            <Route index element={<Categories />} />
-                            <Route path=":idCategory" element={<SingleCat />} />
+                    <Routes>
+                        <Route path="/">
+                            <Route index element={<Landing />} />
+                            <Route path="shops" element={<Shops />} />
+                            <Route path="categories">
+                                <Route index element={<Categories />} />
+                                <Route
+                                    path=":idCategory"
+                                    element={<SingleCat />}
+                                />
+                            </Route>
                         </Route>
-                    </Route>
 
-                    <Route path="/auth" element={<Auth />} />
+                        <Route path="/auth" element={<Auth />} />
 
-                    <Route path="user/:idUser">
-                        <Route index element={<Profile />} />
-                        {ordersNestedRoutes()}
-                        {reviewsNestedRoutes()}
-                    </Route>
-
-                    <Route path="shop/:idShop">
-                        <Route index element={<Shop />} />
-                        {productsNestedRoutes()}
-                        {reviewsNestedRoutes()}
-                        <Route path="dashboard">
-                            <Route index element={<Dashboard />} />
+                        <Route path="user/:idUser">
+                            <Route index element={<Profile />} />
                             {ordersNestedRoutes()}
-                            {productsNestedRoutes()}
+                            {reviewsNestedRoutes()}
                         </Route>
-                    </Route>
 
-                    <Route
-                        path="tests"
-                        element={
-                            <>
-                                <h1>Redux Tests</h1>
-                                <MainTest />
-                                <ReduxTest />
-                                <AxiosTest />
-                            </>
-                        }
-                    />
-                </Routes>
+                        <Route path="shop/:idShop">
+                            <Route index element={<Shop />} />
+                            {productsNestedRoutes()}
+                            {reviewsNestedRoutes()}
+                            <Route path="dashboard">
+                                <Route index element={<Dashboard />} />
+                                {ordersNestedRoutes()}
+                                {productsNestedRoutes()}
+                            </Route>
+                        </Route>
+
+                        <Route
+                            path="tests"
+                            element={
+                                <>
+                                    <h1>Redux Tests</h1>
+                                    <MainTest />
+                                    <ReduxTest />
+                                    <AxiosTest />
+                                </>
+                            }
+                        />
+                    </Routes>
+                </ScrollToTop>
             </BrowserRouter>
         </div>
     )
