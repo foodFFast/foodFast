dd09d4c9bdd65e15f7cb9a853057d7459543683c
Guido Queiroz <Takzzg@hotmail.com>
2022-06-09 01:21:20 -0300

    fix consistency

--- api/src/controllers/storeControllers.js
+++ api/src/controllers/storeControllers.js
@@ -1,30 +1,27 @@
 import Store from "../models/store.js"
 
-export const allStore = async(req,res) =>{
+export const allStore = async (req, res) => {
     try {
         const stores = await Store.find()
-        return res.json({stores})
+        return res.json(stores)
     } catch (error) {
         console.log(error)
-        return res.json({err: 'Error de Servidor'})
+        return res.json({ err: "Error de Servidor" })
     }
 }
 
-export const getStore = async(req,res)=>{
-    
-}
+export const getStore = async (req, res) => {}
 
-export const postStore = async(req,res)=>{
+export const postStore = async (req, res) => {
     try {
-        const {name,img,description,hours} = req.body
-        let store = await Store.findOne({name})
-        if(store) return res.json({msg: "the store is already registered"})
-        store = new Store({name, img, description,hours})
+        const { name, img, description, hours } = req.body
+        let store = await Store.findOne({ name })
+        if (store) return res.json({ msg: "the store is already registered" })
+        store = new Store({ name, img, description, hours })
         await store.save()
-        return res.json({store})
+        return res.json({ store })
     } catch (error) {
         console.log(error)
-        return res.json({err: 'Error de Servidor'})
+        return res.json({ err: "Error de Servidor" })
     }
 }
-
