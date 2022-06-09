84bb318cc50501c804e09e3714107f4a0df0412e
Guido Queiroz <Takzzg@hotmail.com>
2022-06-09 00:05:50 -0300

    switch theme working

---
+++ client/src/redux/reducers/theme.js
@@ -0,0 +1,54 @@
+import { SWITCH_THEME } from "../actions/types"
+
+const themes = {
+    light: {
+        name: "light",
+        colors: {
+            main: "#c9933e",
+            secondary: "",
+            background: "#f5f5f5"
+        },
+        text: {
+            highContrast: "#000000",
+            light: "#ffffff",
+            dark: "#000000"
+        }
+    },
+    dark: {
+        name: "dark",
+        colors: {
+            main: "#c9933e",
+            secondary: "",
+            background: "#161616"
+        },
+        text: {
+            highContrast: "#ffffff",
+            light: "#ffffff",
+            dark: "#000000"
+        }
+    }
+}
+
+const initialState = {
+    selectedTheme: themes.light
+}
+
+const theme = (state = initialState, action) => {
+    let newState = { ...state }
+
+    switch (action.type) {
+        default:
+            break
+
+        case SWITCH_THEME:
+            newState.selectedTheme =
+                state.selectedTheme.name === "light"
+                    ? themes.dark
+                    : themes.light
+            break
+    }
+
+    return { ...newState }
+}
+
+export default theme
