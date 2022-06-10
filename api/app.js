5619d04e6dc1e9322583701879a57b09dfe34d7b
Guido Queiroz <Takzzg@hotmail.com>
2022-06-10 16:44:31 -0300

    remove isProduction

--- api/app.js
+++ api/app.js
@@ -53,12 +53,9 @@ app.use("/api/v1/order", orderRoute)
 //http://localhost:3001/api/v1/user
 app.use("/api/v1/user", userRoute)
 
-isProduction &&
-    app.get("*", function (request, response) {
-        response.sendFile(
-            path.resolve(__dirname, "../client/build", "index.html")
-        )
-    })
+app.get("*", function (request, response) {
+    response.sendFile(path.resolve(__dirname, "../client/build", "index.html"))
+})
 
 dbConn()
 const PORT = process.env.PORT || 3001
