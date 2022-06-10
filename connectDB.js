883e635529500749e410519bf79f477e1e1777ab
Mariano <mariano_fferrer@hotmail.com>
2022-06-09 22:27:04 -0300

    heroku master

--- connectDB.js
+++ connectDB.js
@@ -3,7 +3,6 @@ import mongoose from "mongoose";
 try {
     await mongoose.connect(process.env.MONGODB_CNN);
 
-    mongoose.connection.on('open', ()=>{console.log("connected to DB")});
 
     console.log('connect to db');
 } catch (error) {
