a1525829a5d159d7f7970df36f02018d3845a0d4
Guido Queiroz <Takzzg@hotmail.com>
2022-06-11 00:20:34 -0300

    search shop by param id

--- client/src/components/Shop/Shop.jsx
+++ client/src/components/Shop/Shop.jsx
@@ -1,11 +1,15 @@
 import React from "react"
 import { Link, useParams } from "react-router-dom"
+import { useSelector } from "react-redux"
 
 const Shop = () => {
     const { idShop } = useParams()
+    const shop = useSelector((state) =>
+        state.main.shops.find((s) => s._id === idShop)
+    )
     return (
         <div>
-            <h1>Local numero {idShop}</h1>
+            <h1>Local numero {shop.name}</h1>
             <Link to="products">Ver todos los productos del local</Link> <br />
             <Link to="reviews">Ver todas las reseñas del local</Link> <br />
             <Link to="dashboard">Ir al dashboard</Link>
