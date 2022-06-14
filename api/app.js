2c4d42e9532c31a83cf232ef16874b639ce52342
Guido Queiroz <Takzzg@hotmail.com>
2022-06-13 21:42:35 -0300

    path join

--- api/app.js
+++ api/app.js
@@ -24,7 +24,7 @@ app.use(express.json())
 app.use(cors())
 app.use(morgan("dev"))
 
-app.use(express.static("../client/build/"))
+app.use(express.static(path.join(__dirname, "../client/build/")))
 
 // app.get("*", (req, res) => {
 //     res.sendFile(path.join(__dirname, "../client/build/"))
