f634d475d3ffa9c17e910e6818c33dfb3f527cac
Guido Queiroz <Takzzg@hotmail.com>
2022-06-13 21:38:26 -0300

    app.use client build

--- api/app.js
+++ api/app.js
@@ -24,9 +24,11 @@ app.use(express.json())
 app.use(cors())
 app.use(morgan("dev"))
 
-app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../client/build/"))
-})
+app.use(express.static("../client/build/"))
+
+// app.get("*", (req, res) => {
+//     res.sendFile(path.join(__dirname, "../client/build/"))
+// })
 
 // app.get("/", (req, res) => {
 //     res.send("BIENVENIDOS AL PF.\nEsto es un GET a '/'")
